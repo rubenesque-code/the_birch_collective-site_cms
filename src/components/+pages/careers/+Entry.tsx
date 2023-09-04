2709306bd718a6e4ab35f161cd980ca0129b9185
code-sanchu <a.ruben00001@gmail.com>
2023-09-04 13:05:07 +0100

    ready?

--- src/components/+pages/careers/+Entry.tsx
+++ src/components/+pages/careers/+Entry.tsx
@@ -125,7 +125,7 @@ const InitDbData = ({
   return children({
     page: {
       page: pageQuery.data!,
-      careers: careersQuery.data!.filter((career) => career.id !== "dummy"),
+      careers: careersQuery.data!,
     },
 
     common: {
